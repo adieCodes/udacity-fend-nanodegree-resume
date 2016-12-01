fe1dda1ccd89ac4da354081a94bbbc4fd392b173
Adie Williams <hello@adie.codes>
2016-12-01 13:40:38 +0000

    feat: Build work, project, bio and education objects w/ img

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,134 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+  "name": "Adie Williams",
+  "role": "Front-End Web Developer",
+  "contactInfo": {
+    "mobile": 07525655736,
+    "email": "hello@adie.codes",
+    "twitter": "@adie.codes",
+    "github": "adieCodes",
+    "blog": "http://www.adie.codes",
+    "location": "Sheffield, UK"
+  },
+  "pictureURL": "images/fry.jpg",
+  "welcome": "Front-End Web Developer who uses HTML, CSS" +
+    "and JavaScript to build mobile-first responsive sites." +
+    " I enjoy working with people to gain a clear understanding" +
+    " of their business, requirements and users; and then use " +
+    " this knowledge to create a focused and enjoyable experience.",
+  "skills": ["HTML", "CSS", "JavaScript"]
+};
+
+var work = {
+  "roles": [
+    {
+      "employer": "PeoplePlus Ltd",
+      "title": "Telecoms Analyst",
+      "dates": "June 2012 - Present",
+      "location": "Sheffield, UK",
+      "description": "Started as a temp with responsibility for providing 3rd line support and asset management of 2000 mobile devices (BlackBerry’s, mobiles and dongles).<br>After 6 months, I was made permanent and made solely responsible for supporting telephone lines and systems (Cisco, Mitel, Samsung and Panasonic) for 150 sites. This included co-ordinating new offices/office moves, troubleshooting of lines/phone systems, maintenance, liaising with suppliers, invoicing and feeding into board reports.<br>I was also central in a project to migrate from BlackBerry’s to Nokia Lumia’s. Feeding into contract negotiation and Mobile Device Management requirements, testing and implementation. I am currently heavily involved in a project to move our landline infrastructure from legacy phone systems to Avaya."
+    },
+    {
+      "employer": "Unemployed",
+      "title": "Studying Web Dev",
+      "dates": "Feb 2012-June 2012",
+      "location": "Sheffield, UK",
+      "description": "When I was not job hunting, I spent my time learning about coding, design theory and building my first site for a client. I returned to work as my fiancee was made redundant"
+    },
+    {
+      "employer": "Virgin Media via Hays Recruitment",
+      "title": "Desktop Support Analyst",
+      "dates": "April 2011 - Feb 2012",
+      "location": "Sheffield UK",
+      "description": "The role was to try to fix faulty PDA's and organise replacements, build functional devices to be sent to the Field Tech and produce reports tracking the allocation of the devices as well as support the 2nd Line EUC Analyst when workload permits."
+    }
+  ]
+}
+
+var projects = {
+  "project": [
+    {
+      "title": "Udacity Portfolio Project",
+      "dates": "November 2016",
+      "description": "Responsive portfolio project using Flexbox",
+      "image": "images/logoLine.svg"
+    }
+  ]
+}
+
+var education = {
+  "schools": [
+    {
+      "name": "Bournemouth Uni",
+      "city": "Bournemouth, UK",
+      "course": "HND in Tourism and Leisure Management",
+      "graduated": 2003,
+      "url": "https://www1.bournemouth.ac.uk/"
+    },
+    {
+      "name": "Oakmead Sixth Form",
+      "city": "Bournemouth, UK",
+      "course": "A level Physical Education",
+      "graduated": 2001,
+      "url": "https://oak.leaf.bournemouth.sch.uk/index.php/sixth-form"
+    },
+    {
+      "name": "Oakmead College of Technology",
+      "city": "Bournemouth, UK",
+      "course": ["English", "Mathematics", "Science"],
+      "graduated": 1999,
+      "url": "https://oak.leaf.bournemouth.sch.uk/"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "name": "Front-End Web Developer Nanodegree",
+      "school": "Udacity",
+      "dates": 2016,
+      "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001?v=fe1";
+    },
+    {
+      "name": "Front End Developer Certification",
+      "school": "Free Code Camp",
+      "dates": 2016,
+      "url": "https://www.freecodecamp.com/adieCodes"
+    }
+  ]
+}
+
+HTMLheaderName = HTMLheaderName.replace("%data%", bio.name);
+HTMLheaderRole = HTMLheaderRole.replace("%data%", bio.role);
+HTMLmobile = HTMLmobile.replace("%data%", bio.contactInfo.mobile);
+HTMLemail = HTMLemail.replace("%data%", bio.contactInfo.email);
+HTMLtwitter = HTMLtwitter.replace("%data%", bio.contactInfo.twitter);
+HTMLgithub = HTMLgithub.replace("%data%", bio.contactInfo.github);
+HTMLblog = HTMLblog.replace("%data%", bio.contactInfo.blog);
+HTMLlocation = HTMLlocation.replace("%data%", bio.contactInfo.location);
+HTMLbioPic = HTMLbioPic.replace("%data%", bio.pictureURL);
+HTMLwelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcome);
+var HTMLskill1 = HTMLskills.replace("%data%", bio.skills[0]);
+var HTMLskill2 = HTMLskills.replace("%data%", bio.skills[1]);
+var HTMLskill3 = HTMLskills.replace("%data%", bio.skills[2]);
+HTMLskills = HTMLskill1 + HTMLskill2 + HTMLskill3;
+HTMLworkEmployer = HTMLworkEmployer.replace("%data%", work.employer);
+HTMLworkTitle = HTMLworkTitle.replace("%data%", work.position);
+HTMLworkDates = HTMLworkDates.replace("%data", work.years);
+HTMLworkLocation = HTMLworkLocation.replace("%data%", work.location);
+HTMLschoolName = HTMLschoolName.replace("%data%", education.name);
+HTMLschoolDates = HTMLschoolDates.replace("%data%", education.years);
+HTMLschoolLocation = HTMLschoolLocation.replace("%data%", education.location);
+
+$("#header").prepend(
+  HTMLheaderName,
+  HTMLheaderRole,
+  HTMLmobile,
+  HTMLemail,
+  HTMLtwitter,
+  HTMLgithub,
+  HTMLlocation,
+  HTMLbioPic,
+  HTMLwelcomeMsg,
+  HTMLskills
+);
+
+$("#workExperience").append(HTMLworkStart, HTMLworkTitle);
+$("#education").append(HTMLschoolStart, HTMLschoolName);
