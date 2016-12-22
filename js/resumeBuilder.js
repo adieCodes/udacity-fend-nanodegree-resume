dd0545d302c360d92ea241a133e730ca73c47959
Adie Williams <hello@adie.codes>
2016-12-22 06:26:51 +0000

    fix: Remove test properties from bio.contacts

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,9 +6,7 @@ var bio = {
 		"email": "hello@adie.codes",
 		"github": "adieCodes",
 		"twitter": "@adie.codes",
-		"location": "Sheffield, UK",
-		"test": "test",
-		"more": "more"
+		"location": "Sheffield, UK"
 	},
 	"welcomeMessage": "Front-End Web Developer who uses HTML, CSS and JavaScript to build mobile-first responsive sites. I enjoy working with people to gain a clear understanding of their business, requirements and users; and then use this knowledge to create a focused and enjoyable experience.",
 	"skills": ["HTML", "CSS", "JavaScript"],
